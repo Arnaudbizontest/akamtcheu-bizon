4d1b0896d7e151f6d62f3ce5372491eb600662c4
Arnaud <arnaudkamtcheu.pro@gmail.com>
2019-06-11 15:10:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Accueil from "./components/accueil.component";
 
